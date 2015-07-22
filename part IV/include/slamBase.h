a5d869e85fbc84ce5f161cf74e4dfa682b546f52
zero <zero@zbot.(none)>
2015-07-22 23:01:38 +0800

    ch4

---
+++ part IV/include/slamBase.h
@@ -0,0 +1,113 @@
+/*************************************************************************
+	> File Name: rgbd-slam-tutorial-gx/part III/code/include/slamBase.h
+	> Author: xiang gao
+	> Mail: gaoxiang12@mails.tsinghua.edu.cn
+	> Created Time: 2015年07月18日 星期六 15时14分22秒
+    > 说明：rgbd-slam教程所用到的基本函数（C风格）
+ ************************************************************************/
+# pragma once
+
+// 各种头文件 
+// C++标准库
+#include <fstream>
+#include <vector>
+#include <map>
+using namespace std;
+
+// OpenCV
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/nonfree/nonfree.hpp>
+
+
+//PCL
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+
+// 类型定义
+typedef pcl::PointXYZRGBA PointT;
+typedef pcl::PointCloud<PointT> PointCloud;
+
+// 相机内参结构
+struct CAMERA_INTRINSIC_PARAMETERS 
+{ 
+    double cx, cy, fx, fy, scale;
+};
+
+// 帧结构
+struct FRAME
+{
+    cv::Mat rgb, depth; //该帧对应的彩色图与深度图
+    cv::Mat desp;       //特征描述子
+    vector<cv::KeyPoint> kp; //关键点
+};
+
+// PnP 结果
+struct RESULT_OF_PNP
+{
+    cv::Mat rvec, tvec;
+    int inliers;
+};
+
+// 函数接口
+// image2PonitCloud 将rgb图转换为点云
+PointCloud::Ptr image2PointCloud( cv::Mat& rgb, cv::Mat& depth, CAMERA_INTRINSIC_PARAMETERS& camera );
+
+// point2dTo3d 将单个点从图像坐标转换为空间坐标
+// input: 3维点Point3f (u,v,d)
+cv::Point3f point2dTo3d( cv::Point3f& point, CAMERA_INTRINSIC_PARAMETERS& camera );
+
+// computeKeyPointsAndDesp 同时提取关键点与特征描述子
+void computeKeyPointsAndDesp( FRAME& frame, string detector, string descriptor );
+
+// estimateMotion 计算两个帧之间的运动
+// 输入：帧1和帧2, 相机内参
+RESULT_OF_PNP estimateMotion( FRAME& frame1, FRAME& frame2, CAMERA_INTRINSIC_PARAMETERS& camera );
+
+// 参数读取类
+class ParameterReader
+{
+public:
+    ParameterReader( string filename="./parameters.txt" )
+    {
+        ifstream fin( filename.c_str() );
+        if (!fin)
+        {
+            cerr<<"parameter file does not exist."<<endl;
+            return;
+        }
+        while(!fin.eof())
+        {
+            string str;
+            getline( fin, str );
+            if (str[0] == '#')
+            {
+                // 以‘＃’开头的是注释
+                continue;
+            }
+
+            int pos = str.find("=");
+            if (pos == -1)
+                continue;
+            string key = str.substr( 0, pos );
+            string value = str.substr( pos+1, str.length() );
+            data[key] = value;
+
+            if ( !fin.good() )
+                break;
+        }
+    }
+    string getData( string key )
+    {
+        map<string, string>::iterator iter = data.find(key);
+        if (iter == data.end())
+        {
+            cerr<<"Parameter name "<<key<<" not found!"<<endl;
+            return string("NOT_FOUND");
+        }
+        return iter->second;
+    }
+public:
+    map<string, string> data;
+};
---
+++ part IV/src/slamBase.cpp
@@ -0,0 +1,151 @@
+/*************************************************************************
+	> File Name: src/slamBase.cpp
+	> Author: xiang gao
+	> Mail: gaoxiang12@mails.tsinghua.edu.cn
+    > Implementation of slamBase.h
+	> Created Time: 2015年07月18日 星期六 15时31分49秒
+ ************************************************************************/
+
+#include "slamBase.h"
+
+PointCloud::Ptr image2PointCloud( cv::Mat& rgb, cv::Mat& depth, CAMERA_INTRINSIC_PARAMETERS& camera )
+{
+    PointCloud::Ptr cloud ( new PointCloud );
+
+    for (int m = 0; m < depth.rows; m++)
+        for (int n=0; n < depth.cols; n++)
+        {
+            // 获取深度图中(m,n)处的值
+            ushort d = depth.ptr<ushort>(m)[n];
+            // d 可能没有值，若如此，跳过此点
+            if (d == 0)
+                continue;
+            // d 存在值，则向点云增加一个点
+            PointT p;
+
+            // 计算这个点的空间坐标
+            p.z = double(d) / camera.scale;
+            p.x = (n - camera.cx) * p.z / camera.fx;
+            p.y = (m - camera.cy) * p.z / camera.fy;
+            
+            // 从rgb图像中获取它的颜色
+            // rgb是三通道的BGR格式图，所以按下面的顺序获取颜色
+            p.b = rgb.ptr<uchar>(m)[n*3];
+            p.g = rgb.ptr<uchar>(m)[n*3+1];
+            p.r = rgb.ptr<uchar>(m)[n*3+2];
+
+            // 把p加入到点云中
+            cloud->points.push_back( p );
+        }
+    // 设置并保存点云
+    cloud->height = 1;
+    cloud->width = cloud->points.size();
+    cloud->is_dense = false;
+
+    return cloud;
+}
+
+cv::Point3f point2dTo3d( cv::Point3f& point, CAMERA_INTRINSIC_PARAMETERS& camera )
+{
+    cv::Point3f p; // 3D 点
+    p.z = double( point.z ) / camera.scale;
+    p.x = ( point.x - camera.cx) * p.z / camera.fx;
+    p.y = ( point.y - camera.cy) * p.z / camera.fy;
+    return p;
+}
+
+// computeKeyPointsAndDesp 同时提取关键点与特征描述子
+void computeKeyPointsAndDesp( FRAME& frame, string detector, string descriptor )
+{
+    cv::Ptr<cv::FeatureDetector> _detector;
+    cv::Ptr<cv::DescriptorExtractor> _descriptor;
+
+    cv::initModule_nonfree();
+    _detector = cv::FeatureDetector::create( detector.c_str() );
+    _descriptor = cv::DescriptorExtractor::create( descriptor.c_str() );
+
+    if (!_detector || !_descriptor)
+    {
+        cerr<<"Unknown detector or discriptor type !"<<detector<<","<<descriptor<<endl;
+        return;
+    }
+
+    _detector->detect( frame.rgb, frame.kp );
+    _descriptor->compute( frame.rgb, frame.kp, frame.desp );
+
+    return;
+}
+
+// estimateMotion 计算两个帧之间的运动
+// 输入：帧1和帧2
+// 输出：rvec 和 tvec
+RESULT_OF_PNP estimateMotion( FRAME& frame1, FRAME& frame2, CAMERA_INTRINSIC_PARAMETERS& camera )
+{
+    static ParameterReader pd;
+    vector< cv::DMatch > matches;
+    cv::FlannBasedMatcher matcher;
+    matcher.match( frame1.desp, frame2.desp, matches );
+   
+    cout<<"find total "<<matches.size()<<" matches."<<endl;
+    vector< cv::DMatch > goodMatches;
+    double minDis = 9999;
+    double good_match_threshold = atof( pd.getData( "good_match_threshold" ).c_str() );
+    for ( size_t i=0; i<matches.size(); i++ )
+    {
+        if ( matches[i].distance < minDis )
+            minDis = matches[i].distance;
+    }
+
+    for ( size_t i=0; i<matches.size(); i++ )
+    {
+        if (matches[i].distance < good_match_threshold*minDis)
+            goodMatches.push_back( matches[i] );
+    }
+
+    cout<<"good matches: "<<goodMatches.size()<<endl;
+    // 第一个帧的三维点
+    vector<cv::Point3f> pts_obj;
+    // 第二个帧的图像点
+    vector< cv::Point2f > pts_img;
+
+    // 相机内参
+    for (size_t i=0; i<goodMatches.size(); i++)
+    {
+        // query 是第一个, train 是第二个
+        cv::Point2f p = frame1.kp[goodMatches[i].queryIdx].pt;
+        // 获取d是要小心！x是向右的，y是向下的，所以y才是行，x是列！
+        ushort d = frame1.depth.ptr<ushort>( int(p.y) )[ int(p.x) ];
+        if (d == 0)
+            continue;
+        pts_img.push_back( cv::Point2f( frame2.kp[goodMatches[i].trainIdx].pt ) );
+
+        // 将(u,v,d)转成(x,y,z)
+        cv::Point3f pt ( p.x, p.y, d );
+        cv::Point3f pd = point2dTo3d( pt, camera );
+        pts_obj.push_back( pd );
+    }
+
+    double camera_matrix_data[3][3] = {
+        {camera.fx, 0, camera.cx},
+        {0, camera.fy, camera.cy},
+        {0, 0, 1}
+    };
+
+    cout<<"solving pnp"<<endl;
+    // 构建相机矩阵
+    cv::Mat cameraMatrix( 3, 3, CV_64F, camera_matrix_data );
+    cv::Mat rvec, tvec, inliers;
+    // 求解pnp
+    cv::solvePnPRansac( pts_obj, pts_img, cameraMatrix, cv::Mat(), rvec, tvec, false, 100, 1.0, 100, inliers );
+
+    RESULT_OF_PNP result;
+    // 当心这个clone!! 很坑的！
+    // 不用clone，r,t里的值会被当作局部变量扔掉，返回的时候就没有值了
+    result.rvec = rvec;
+    result.tvec = tvec;
+    //result.rvec = rvec.clone();
+    //result.tvec = tvec.clone();
+    result.inliers = inliers.rows;
+
+    return result;
+}
