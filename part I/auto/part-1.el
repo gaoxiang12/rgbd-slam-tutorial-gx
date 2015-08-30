e6dec93a590549d6efd54adc946f265cc484df2e
gaoxiang12 <gaoxiang12@mails.tsinghua.edu.cn>
2015-08-30 14:06:02 +0800

    add g2o.zip

---
+++ part I/auto/part-1.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook "part-1"
+ (lambda ()
+    (TeX-run-style-hooks
+     "framed"
+     "listings"
+     "indentfirst"
+     "fontspec"
+     "float"
+     "geometry"
+     "latex2e"
+     "art10"
+     "article"
+     "9pt"
+     "a4paper")))
+
